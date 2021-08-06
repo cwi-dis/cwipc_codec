b7b4da39a89788a03860fc1dccc872ab87ced6de
Jack Jansen <Jack.Jansen@cwi.nl>
2021-08-06 16:45:49 +0200

    Adapted to do parallel encoding.

--- apps/cwipc_enc_perftest/cwipc_enc_perftest.cpp
+++ apps/cwipc_enc_perftest/cwipc_enc_perftest.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <vector>
 
 #include "cwipc_codec/api.h"
 
-#define COUNT 10
-#define OCTREE_BITS 9
-#define JPEG_QUALITY 85
-#define TILENUMBER 2
+#define COUNT 50
+
+std::vector<int> all_octree_bits { 6, 9 };
+std::vector<int> all_jpeg_quality { 85 };
+std::vector<int> all_tilenumber { 1, 2, 3, 4 };
 
 int main(int argc, char** argv)
 {
@@ -34,10 +36,10 @@ int main(int argc, char** argv)
 	param.do_inter_frame = false;
 	param.gop_size = 1;
 	param.exp_factor = 1.0;
-	param.octree_bits = OCTREE_BITS;
-	param.jpeg_quality = JPEG_QUALITY;
+	param.octree_bits = 0;
+	param.jpeg_quality = 0;
 	param.macroblock_size = 16;
-	param.tilenumber = TILENUMBER;
+	param.tilenumber = 0;
 
 	char *errorString;
     cwipc_encodergroup *encodergroup = cwipc_new_encodergroup(&errorString, CWIPC_API_VERSION);
@@ -45,35 +47,51 @@ int main(int argc, char** argv)
         std::cerr << argv[0] << ": Could not create encodergroup: " << errorString << std::endl;
         return 1;
     }
-    cwipc_encoder *encoder = encodergroup->addencoder(CWIPC_ENCODER_PARAM_VERSION, &param, &errorString);
-    if (encoder == NULL) {
-    	std::cerr << argv[0] << ": Could not create encoder: " << errorString << std::endl;
-    	return 1;
+    std::vector<cwipc_encoder *> encoders;
+    for(int octree_bits : all_octree_bits) {
+        for(int jpeg_quality : all_jpeg_quality) {
+            for(int tilenumber : all_tilenumber) {
+                param.octree_bits = octree_bits;
+                param.jpeg_quality = jpeg_quality;
+                param.tilenumber = tilenumber;
+                cwipc_encoder *e = encodergroup->addencoder(CWIPC_ENCODER_PARAM_VERSION, &param, &errorString);
+                if (e == NULL) {
+                    std::cerr << argv[0] << ": Could not create encoder: " << errorString << std::endl;
+                    return 1;
+                }
+                encoders.push_back(e);
+            }
+        }
     }
     
     auto t0_cpu = std::clock();
     auto t0_wall = std::chrono::high_resolution_clock::now();
-    size_t bufSize;
+    size_t totalBufSize = 0;
+    int totalCount = 0;
     
     for (int i=0; i<COUNT; i++) {
-    	encoder->feed(pc);
-		bool ok = encoder->available(true);
-		if (!ok) {
-			std::cerr << argv[0] << ": Encoder did not create compressed data" << std::endl;
-			return 1;
-		}
-        bufSize = encoder->get_encoded_size();
-		char *buffer = (char *)malloc(bufSize);
-		if (buffer == NULL) {
-			std::cerr << argv[0] << ": Could not allocate " << bufSize << " bytes." << std::endl;
-			return 1;
-		}
-		ok = encoder->copy_data(buffer, bufSize);
-		if (!ok) {
-			std::cerr << argv[0] << ": Encoder could not copy compressed data" << std::endl;
-			return 1;
-		}
-		free(buffer);
+    	encodergroup->feed(pc);
+        for(cwipc_encoder *e: encoders) {
+            bool ok = e->available(true);
+            if (!ok) {
+                std::cerr << argv[0] << ": Encoder did not create compressed data" << std::endl;
+                return 1;
+            }
+            size_t bufSize = e->get_encoded_size();
+            totalBufSize += bufSize;
+            totalCount++;
+            char *buffer = (char *)malloc(bufSize);
+            if (buffer == NULL) {
+                std::cerr << argv[0] << ": Could not allocate " << bufSize << " bytes." << std::endl;
+                return 1;
+            }
+            ok = e->copy_data(buffer, bufSize);
+            if (!ok) {
+                std::cerr << argv[0] << ": Encoder could not copy compressed data" << std::endl;
+                return 1;
+            }
+            free(buffer);
+        }
     }
     
     auto t1_cpu = std::clock();
@@ -83,7 +101,7 @@ int main(int argc, char** argv)
     double delta_wall = std::chrono::duration<double, std::milli>(t1_wall - t0_wall).count();
 	delta_cpu /= COUNT;
 	delta_wall /= COUNT;
-    std::cerr << argv[0] << ": Compressed " << COUNT << " times, output: " << bufSize << " bytes" << std::endl;
+    std::cerr << argv[0] << ": Compressed " << COUNT << " times, output: " << totalBufSize/COUNT << " bytes per input cloud, " << totalBufSize/totalCount << " average per output packet" << std::endl;
     std::cerr << std::fixed << std::setprecision(2) << argv[0] << ": per iteration: cpu: " <<  delta_cpu << " ms, real: " << delta_wall << "ms" << std::endl;
 
     pc->free();	// After feeding the pointcloud into the encoder we can free it.
