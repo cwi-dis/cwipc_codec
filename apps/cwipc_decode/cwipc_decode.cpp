f23a9c51ed2932a487089f760abf185450d5eb1d
itamayo <tamaxx@gmail.com>
2024-03-14 10:05:03 +0100

    [fix]:encode/decode app output help
    # cwipc_decode 
    Usage: cwipc_decodecompressedfile.cwicpc pointcloudfile.ply
    Missing blank space between binary an arguments.
    # cwipc_decode 
    Usage: cwipc_decode compressedfile.cwicpc pointcloudfile.ply

--- apps/cwipc_decode/cwipc_decode.cpp
+++ apps/cwipc_decode/cwipc_decode.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << "compressedfile.cwicpc pointcloudfile.ply" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " compressedfile.cwicpc pointcloudfile.ply" << std::endl;
         return 2;
     }
 
