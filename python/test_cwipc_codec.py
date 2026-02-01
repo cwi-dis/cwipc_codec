cb3e46313827fea3a14cc230f73581478e4bc572
Jack Jansen <Jack.Jansen@cwi.nl>
2026-02-01 22:31:55 +0100

    Tests adapted for start() method.

--- python/test_cwipc_codec.py
+++ python/test_cwipc_codec.py
@@ -220,6 +220,7 @@ class TestApi(unittest.TestCase):
     def test_cwipc_multiencoder_octree_depth(self):
         """Test that a multiencoder setup for 2 octree depths returns sensible pointclouds"""
         gen = cwipc.cwipc_synthetic()
+        gen.start()
         pc_orig = gen.get()
         self.assertIsNotNone(pc_orig)
         assert pc_orig # Only to keep linters happy
@@ -242,6 +243,7 @@ class TestApi(unittest.TestCase):
     def test_cwipc_parallel_encoder(self):
         """Test that a parallel encoder returns the same compressed data as a non-parallel one"""       
         gen = cwipc.cwipc_synthetic()
+        gen.start()
         params1 = _cwipc_codec.cwipc_new_encoder_params()
         params2 = _cwipc_codec.cwipc_new_encoder_params()
         enc1 = _cwipc_codec.cwipc_new_encoder(params=params1)
